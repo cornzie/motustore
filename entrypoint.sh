a8c2d8ab5964ea82c0b8ba7a012e826e44a1e0ae
Cornelius <cornelius.udeh@yahoo.com>
2023-10-23 16:02:31 +0200

    fix: dev environment auto pilot setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ if [ ! -f ".env" ]
 then
     echo "Creating env file for $APP_ENV"
     cp .env.example .env
-    php artisan key:generate
 else
     echo ".env exists"
 fi
@@ -18,13 +17,14 @@ role=${CONTAINER_APP:-app}
 
 if [ "$role" = "app" ]
 then
-    php artisan migrate --seed
-    php artisan optimize:clear
-
     npm install -g npm@latest
     npm install
     npm audit fix
     npm run build
+    
+    php artisan key:generate
+    php artisan optimize:clear
+    php artisan migrate --seed
 
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
